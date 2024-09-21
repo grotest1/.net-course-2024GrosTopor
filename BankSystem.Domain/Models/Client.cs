aaea3d86daad8d1038626583b9f987ecbd9b5210
gros_nout <01grost@mail.ru>
2024-09-21 15:49:09 +0300

    3

--- BankSystem.Domain/Models/Client.cs
+++ BankSystem.Domain/Models/Client.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankSystem.Domain.Models
 {
-    internal class Client
+    internal class Client : Person
     {
     }
 }
--- BankSystem.Domain/Models/Employee.cs
+++ BankSystem.Domain/Models/Employee.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankSystem.Domain.Models
 {
-    internal class Employee
+    internal class Employee : Person
     {
     }
 }
