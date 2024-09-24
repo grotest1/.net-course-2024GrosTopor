1763e6966bbf583cc4510f316dde6e79e61c0da8
Grost_home <01seroga@mail.ru>
2024-09-24 08:30:02 +0300

    +salary

--- BankSystem.App/Services/TestDataGenerator.cs
+++ BankSystem.App/Services/TestDataGenerator.cs
@@ -17,7 +17,7 @@ namespace BankSystem.App.Services
 
             for (int i = 0; i < 1000; i++)
             {
-                SetRandomePersonData(rnd, out string name, out DateOnly birthday, out string phoneNumber);
+                SetRandomePersonData(rnd, out string name, out DateOnly birthday, out string phoneNumber, out int salary);
                 resultList.Add(new Client() { Name = name, Birthday = birthday, PersonalPhoneNumber = phoneNumber });
             }
 
@@ -32,7 +32,7 @@ namespace BankSystem.App.Services
 
             for (int i = 0; i < 1000; i++)
             {
-                SetRandomePersonData(rnd, out string name, out DateOnly birthday, out string phoneNumber);
+                SetRandomePersonData(rnd, out string name, out DateOnly birthday, out string phoneNumber, , out int salary);
                 Client client = new Client() { Name = name, Birthday = birthday, PersonalPhoneNumber = phoneNumber };                
                 resultDictionary.TryAdd(phoneNumber, client);
             }
@@ -48,14 +48,14 @@ namespace BankSystem.App.Services
 
             for (int i = 0; i < 1000; i++)
             {
-                SetRandomePersonData(rnd, out string name, out DateOnly birthday, out string phoneNumber);
+                SetRandomePersonData(rnd, out string name, out DateOnly birthday, out string phoneNumber, out int salary);
                 result_list.Add(new Employee() { Name = name, Birthday = birthday, PersonalPhoneNumber = phoneNumber });
             }
 
             return result_list;
         }
 
-        private static void SetRandomePersonData(Random rnd, out string name, out DateOnly birthday, out string phoneNumber)
+        private static void SetRandomePersonData(Random rnd, out string name, out DateOnly birthday, out string phoneNumber, out int salary)
         {
             string[] names = {"Вася", "Петя", "Дима", "Сергей", "Костя", "Никита", "Саша", "Жора", "Максим"};
             name = names[rnd.Next(0, names.Length - 1)];
@@ -67,6 +67,8 @@ namespace BankSystem.App.Services
             phoneNumber = phoneCodes[rnd.Next(0, phoneCodes.Length - 1)];
             while (phoneNumber.Length < 8)
                 phoneNumber += rnd.Next(1, 9).ToString();
+
+            salary = rnd.Next(0, 10_000);
         }
 
     }
