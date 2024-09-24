9c8172a69c055361a1080961ad64dc940f8d7c3c
Grost_home <01seroga@mail.ru>
2024-09-24 07:22:23 +0300

    2: 3/5

--- Practice/Practice.cs
+++ Practice/Practice.cs
@@ -8,49 +8,44 @@ internal class Program
 {
 
     private static void Main(string[] args)
-    {
+    {    
+        Stopwatch stopwatch = new Stopwatch();
 
-        #region RefAndValTypes
-
-        Employee employee_1 = new Employee() {Name = "Ионел", Birthday = new DateOnly(1999, 9, 9)};
-        UpdateEmployeeContract(employee_1);
-
-
-        Currency rub = new Currency();
-        UpdateCurrency(ref rub);
-
-
-        Client client = new Client();
-        client.Name = "Иннокентий";
-        var employee_2 = BankService.SetClientAsEmployee(client);
-
-
-        Employee[] employees = { employee_1, employee_2};
-        BankService.CalculateSalary(90_000, 14_000, employees);
-
-
-        void UpdateEmployeeContract(Employee employee)
-        {
-            employee.Contract = $"Contract for {employee.Name} ...";
-        }
-
-        void UpdateCurrency(ref Currency currency)
-        {
-            currency.Name = "Rub";
-            currency.Code = 456;
-        }
-
-        #endregion
-
-    
         List<Client> clientsList = TestDataGenerator.GenerateClientList1000();
         Dictionary<string, Client> clientsDictionary = TestDataGenerator.GenerateClientDictionary1000();
         List<Employee> employeesList = TestDataGenerator.GenerateEmployeeList1000();
 
 
-        Stopwatch stopwatch = new Stopwatch();
+        string phone = "777122345";
+        Client clientToFind = new Client { PersonalPhoneNumber =  phone};
+        clientsList.Add(clientToFind);
+        clientsDictionary.Add(phone, clientToFind);
 
-    
-    
+
+        stopwatch.Start();
+        var result1 = clientsList.FirstOrDefault(c => c.PersonalPhoneNumber == phone);
+        stopwatch.Stop();
+        long duractionListFindInMS = stopwatch.ElapsedMilliseconds;
+
+
+        stopwatch.Restart();
+        var result2 = clientsDictionary[phone];
+        stopwatch.Stop();
+        long duractionDictionaryFindInMS = stopwatch.ElapsedMilliseconds;
+
+
+        //stopwatch.Restart();
+        //var result2 = clientsDictionary[phone];
+        //stopwatch.Stop();
+        //long duractionDictionaryFindInMS = stopwatch.ElapsedMilliseconds;
+
+
+        stopwatch.Restart();
+        var result4 = employeesList.Where(e => e.Salary > 0).OrderBy(e => e.Salary).FirstOrDefault();
+        stopwatch.Stop();
+        long duractionEmployeeFindInMS = stopwatch.ElapsedMilliseconds;
+
+
+
     }
 }
