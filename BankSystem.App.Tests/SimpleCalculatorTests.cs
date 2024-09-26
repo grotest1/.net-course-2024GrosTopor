e3238aa0121fe98445ce44ebf6b4f0e8c1b9135d
gros_nout <01grost@mail.ru>
2024-09-26 19:10:32 +0300

    1

---
+++ BankSystem.App.Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using BankSystem.App;
+
+
+namespace SemplForUnitTest.Tests
+{
+    public class SimpleCalculatorTests
+    {
+        [Fact]
+        public void GetSum_2_Plus_5_Eq_7()
+        {
+            //Arrange
+            SimpleCalculator calculator = new SimpleCalculator();
+            //Act
+            int result = calculator.GetSum(2, 5);
+            //Assert
+            Assert.Equal(result, 7);
+        }
+        [Fact]
+        public void GetSubstraction_5_Minus_4_Eq_1()
+        {
+            //Arrange
+            SimpleCalculator calculator = new SimpleCalculator();
+            //Act
+            int result = calculator.GetSubstraction(5, 4);
+            //Assert
+            Assert.Equal(result, 1);
+        }
+    }
+}
---
+++ BankSystem.App/SimpleCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.App
+{
+    public class SimpleCalculator
+    {
+        public int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+        public int GetSubstraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int GetMultiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public int Getdivision(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
