7695c2765cd9eaf0d6bc3b61cbc76d8b734ad49c
gros_nout <01grost@mail.ru>
2024-10-19 12:29:48 +0300

    реализовал WritePersonsToCsv (базовый вариант) реализовал тест для метода

---
+++ ExportTools/ExportService.cs
@@ -0,0 +1,56 @@
+﻿using BankSystem.Domain.Models;
+using CsvHelper;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace ExportTool
+{
+    public static class ExportService
+    {
+
+        public static void WriteClientsToCsv(List<Client> persons, string[] paths)
+        {
+
+            if (paths.Length == 0)
+                return;
+
+            string[] dirPath = new string[paths.Length - 1];
+            for (int i = 0; i < paths.Length - 1; i++)
+            {
+                dirPath[i] = paths[i];
+            }
+
+            DirectoryInfo dirInfo = new DirectoryInfo(Path.Combine(dirPath));
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+
+            string fullPath = Path.Combine(paths);
+            using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fileStream))
+                {
+                    using (var writer = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                    {
+                        writer.WriteField(nameof(Person.Name));
+                        writer.WriteField(nameof(Person.Age));
+
+                        writer.NextRecord();
+
+                        foreach (Person person in persons)
+                        {
+                            writer.WriteField(person.Name);
+                            writer.WriteField(person.Age);
+                            writer.NextRecord();
+                        }
+
+                        writer.Flush();
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ ExportToolsTests/ExportServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using BankSystem.Domain.Models;
+using ExportTool;
+
+namespace ExportToolsTests
+{
+    public class ExportServiceTests
+    {
+
+        [Fact]
+        public void WritePersonsToCsvPositiveTest()
+        {
+            List<Client> clients = new List<Client>();
+            clients.Add(new Client() { Name = "fdsfsa", Age = 55 });
+            string[] path = { "C:", "1", "2", "test.csv" };
+
+            ExportService.WriteClientsToCsv(clients, path);
+
+
+        }
+
+    }
+}
