f5ad17048037fb758c525a32cfa38a3e63c7d7bc
gros_nout <01grost@mail.ru>
2024-10-24 19:13:22 +0300

    начало ThreadAndTaskTests

---
+++ ExportToolsTests/ThreadAndTaskTests.cs
@@ -0,0 +1,30 @@
+﻿using BankSystem.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BankSystem.Domain.Models;
+using ExportTool;
+using BankSystem.App.Services;
+using BankSystem.Data.Storages;
+
+namespace ExportToolsTests
+{
+    public class ThreadAndTaskTests
+    {
+        [Fact]
+        public void СonveyorProcessingClients()
+        {
+            List <Client> clients = TestDataGenerator.GenerateClientList(10000);
+
+
+
+
+
+        }
+
+
+    }
+}
