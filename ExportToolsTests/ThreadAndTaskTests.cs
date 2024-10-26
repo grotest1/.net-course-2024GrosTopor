304e71a6399044b4e1317dddc66669cb13541776
Grost_home <01seroga@mail.ru>
2024-10-26 16:40:33 +0300

    + ParallelAddingToAccount подшлифовал прошлые методы

--- ExportToolsTests/ThreadAndTaskTests.cs
+++ ExportToolsTests/ThreadAndTaskTests.cs
@@ -1,15 +1,8 @@
 ﻿using BankSystem.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 using ExportTool;
 using BankSystem.App.Services;
-using BankSystem.Data.Storages;
 using Newtonsoft.Json;
-using System.IO;
 
 namespace ExportToolsTests
 {
@@ -18,57 +11,35 @@ namespace ExportToolsTests
         [Fact]
         public void СonveyorProcessingClients()
         {
-            List<Client> clients1 = TestDataGenerator.GenerateClientList(500);
-            List<Client> clients2 = TestDataGenerator.GenerateClientList(500);
-            List<Client> clients3 = TestDataGenerator.GenerateClientList(500);
+            List<Client> clients1 = TestDataGenerator.GenerateClientList(100);
+            List<Client> clients2 = TestDataGenerator.GenerateClientList(200);
+            List<Client> clients3 = TestDataGenerator.GenerateClientList(300);
             int countClients = clients1.Count + clients2.Count + clients3.Count;
 
             Queue<Client> conveyor = [];
+            int addedToConveyor = 0;
 
             var lockerСonveyor = new object();
 
-            int queue = 0;
-
             Random pauseMS = new Random();
-
-            ThreadPool.QueueUserWorkItem(_ =>
+            WaitCallback waitCallback = delegate (object? clientsObj)
             {
-                for (int index1 = 0; index1 < clients1.Count; index1++) {
-                    lock (lockerСonveyor)
+                if (clientsObj is List<Client> clients)
+                {
+                    for (int i = 0; i < clients.Count; i++)
                     {
-                        conveyor.Enqueue(clients1[index1]);
-                        queue++;
+                        lock (lockerСonveyor)
+                        {
+                            conveyor.Enqueue(clients[i]);
+                            addedToConveyor++;
+                        }
+                        Thread.Sleep(pauseMS.Next(10, 300));
                     }
-                    Thread.Sleep(pauseMS.Next(100, 500));
                 }
-
-            });            
-            
-            ThreadPool.QueueUserWorkItem(_ =>
-            {
-                for (int index2 = 0; index2 < clients2.Count; index2++) {
-                    lock (lockerСonveyor)
-                    {
-                        conveyor.Enqueue(clients2[index2]);
-                        queue++;
-                    }
-                    Thread.Sleep(pauseMS.Next(100, 500));
-                }
-
-            });            
-            
-            ThreadPool.QueueUserWorkItem(_ =>
-            {
-                for (int index3 = 0; index3 < clients3.Count; index3++) {
-                    lock (lockerСonveyor)
-                    {
-                        conveyor.Enqueue(clients3[index3]);
-                        queue++;
-                    }
-                    Thread.Sleep(pauseMS.Next(100, 500));
-                }
-
-            });
+            };
+            ThreadPool.QueueUserWorkItem(waitCallback, clients1);
+            ThreadPool.QueueUserWorkItem(waitCallback, clients2);
+            ThreadPool.QueueUserWorkItem(waitCallback, clients3);
 
             string[] pathToDirectory = ["C:", "1", "2"];
             DirectoryInfo dirInfo = new DirectoryInfo(Path.Combine(pathToDirectory));
@@ -77,13 +48,13 @@ namespace ExportToolsTests
                 dirInfo.Create();
             }
 
-
-            int countAdd = 0;
+            int addedToFiles = 0;
             int fileCounter = 0;
             long fileStreamLength = 0;
+            
             List<Client> clientsToFile = new List<Client>();
 
-            while (queue < countClients || conveyor.Count > 0)
+            while (addedToConveyor < countClients || conveyor.Count > 0)
             {
                 while (conveyor.Count == 0)
                     Thread.Sleep(200);
@@ -100,26 +71,24 @@ namespace ExportToolsTests
                 }
 
                 string fullPath = Path.Combine(dirInfo.FullName, $"conveyor_{fileCounter.ToString()}.json");
-
-                using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
+                
+                using (FileStream fileStream = new FileStream(fullPath, File.Exists(fullPath) ? FileMode.Truncate : FileMode.Create))
                 {
                     using (StreamWriter streamWriter = new StreamWriter(fileStream))
                     {
-                        streamWriter.Write(JsonConvert.SerializeObject(clientsToFile)); 
-                        countAdd++;
+                        streamWriter.WriteLine(JsonConvert.SerializeObject(clientsToFile)); 
+                        addedToFiles++;
                         fileStreamLength = streamWriter.BaseStream.Length;
                     }
                 }
-                
             }
 
-            Assert.True(countAdd == countClients);
+            Assert.True(addedToFiles == countClients);
         }
 
         [Fact]
         public void TestСonveyorProcessingClients()
         {
-
             string[] pathToDirectory = ["C:", "1", "2"];
             DirectoryInfo dirInfo = new DirectoryInfo(Path.Combine(pathToDirectory));
             Assert.True(dirInfo.Exists);
@@ -133,6 +102,36 @@ namespace ExportToolsTests
                     Assert.NotEmpty(clients);
                 }
             }
+        }
+
+        [Fact]
+        public void ParallelAddingToAccount()
+        {
+            Account account = new Account();
+            var locker = new Object();
+            int completed = 0;
+
+            Random pauseMS = new Random();
+            WaitCallback waitCallback = delegate (object? _)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    lock (locker)
+                    {
+                        account.Amount += 100;
+                    }
+                    Thread.Sleep(pauseMS.Next(10, 200));
+                }
+                completed++;
+            };
+
+            ThreadPool.QueueUserWorkItem(waitCallback);
+            ThreadPool.QueueUserWorkItem(waitCallback);
+
+            while (completed < 2)
+                Thread.Sleep(100);
+               
+            Assert.True(account.Amount == 2000);
         }
 
     }
