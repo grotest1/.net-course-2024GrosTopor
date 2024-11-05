5fd63a6671cd529b7a73e685eb716a44415e1da3
Grost_home <01seroga@mail.ru>
2024-11-05 21:28:30 +0200

    добавил класс CurrencyService, добавил тест CurrencyServiceTests, добавил модель CurrencyConversionsResponce

---
+++ BancSystem.App.Tests/CurrencyServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using BankSystem.App.Services;
+using Xunit;
+
+using BankSystem.Domain.Models;
+using BankSystem.App.Exceptions;
+using BankSystem.Data.Storages;
+
+namespace BancSystem.App.Tests
+{
+    public class CurrencyServiceTests
+    {
+        [Fact]
+        public async Task CurrencyConversionAsyncPositiveTestAsync1()
+        {
+            float result = await CurrencyService.CurrencyConversionAsync("USD", "USD", 100);
+
+            Assert.Equal(100, result);
+        }
+
+        [Fact]
+        public async Task CurrencyConversionAsyncPositiveTestAsync2()
+        {
+            float result = await CurrencyService.CurrencyConversionAsync("USD", "MDL", 100);
+
+            Assert.True(result > 1700);
+        }
+
+        [Fact]
+        public async Task CurrencyConversionAsyncNegativeTestAsync()
+        {
+            await Assert.ThrowsAsync<HttpRequestException>(() => CurrencyService.CurrencyConversionAsync("USDDDD", "MDLLLLL", 100));
+        }
+
+    }
+}
---
+++ BankSystem.App/Services/CurrencyService.cs
@@ -0,0 +1,52 @@
+﻿using BankSystem.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using BankSystem.App.HttpData;
+
+namespace BankSystem.App.Services
+{
+    public static class CurrencyService
+    {
+        public static async Task<float> CurrencyConversionAsync(string currencyTitleFrom, string currencyTitleTo, float amount = 1)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var parameters = new Dictionary<string, string>();
+                parameters.Add("api_key", "YvQNW3U5Q6dtwjXpXPyJmYCpwxbBah");
+                parameters.Add("from", currencyTitleFrom);
+                parameters.Add("to", currencyTitleTo);
+                if (amount != 1)
+                    parameters.Add("amount", Convert.ToString(amount));
+                
+                string queryString = string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"));
+                string fullUri = "https://www.amdoren.com/api/currency.php" + "?" + queryString;
+
+                HttpResponseMessage response = await httpClient.GetAsync(fullUri);
+
+                response.EnsureSuccessStatusCode();
+
+                string message = await response.Content.ReadAsStringAsync();
+
+                CurrencyConversionsResponce? currencyConversionsResponce = JsonConvert.DeserializeObject<CurrencyConversionsResponce>(message);
+
+                if (currencyConversionsResponce != null)
+                {
+                    if (currencyConversionsResponce.Error == 0)
+                        return currencyConversionsResponce.Amount;
+                    else
+                        throw new HttpRequestException(currencyConversionsResponce.ErrorMessage);
+                }
+                else
+                    throw new JsonException("Ошибка конвертации ответа");
+
+            }
+        }
+
+    }
+}
