f70f0cb0086393c5895f31905b75fd73d05532a4
gros_nout <01grost@mail.ru>
2024-11-13 22:04:27 +0200

    ytr

--- BankSystem.Data/BankSystemDbContext.cs
+++ BankSystem.Data/BankSystemDbContext.cs
@@ -13,35 +13,8 @@ namespace BankSystem.Data
         public DbSet<Account> Accounts => Set<Account>();
         public DbSet<Currency> Currencies => Set<Currency>();
 
-        //public BankSystemDbContext()
-        //{
 
-        //}
-        //public BankSystemDbContext(IConfiguration configuration)
-        //{
-        //    var c = 1;
-        //}
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-
-            //var builder = new ConfigurationBuilder();
-            // установка пути к текущему каталогу
-            //builder.SetBasePath(Directory.GetCurrentDirectory());
-            //// получаем конфигурацию из файла appsettings.json
-            
-            
-            
-            //builder.AddJsonFile("appsettings.json");
-            //// создаем конфигурацию
-            //var config = builder.Build();
-            //// получаем строку подключения
-            //var connectionString = config.GetConnectionString("DefaultConnection");
-
-            //optionsBuilder.UseNpgsql(connectionString);
-
-            optionsBuilder.UseNpgsql("Host=localhost; Port = 5432; Database = postgres; Username = pg2; Password = pg2");
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
